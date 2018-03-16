11c6f4113c99557a4d1667fd312aec65c4334e28
a.pochtovaya <2111.alisa@gmail.ru>
2018-03-16 12:05:39 +0300

    staff in menu

--- src/views/AdminPage.tsx
+++ src/views/AdminPage.tsx
@@ -14,11 +14,12 @@ export default class AdminPage extends React.Component<Props,any> {
         const {onLogin , user} = this.props;
         return(
             <div className="container-fluid">
-                <Header onLogin={onLogin} user = {user} links_after_auth={true}/>
+                <Header onLogin={onLogin} user = {user} links_after_auth={true} search={true}/>
                 <div className="row">
-                    <div className="col-2">
+                    <div className="col-2 menu">
+                        <h1 className='header-menu'>Меню</h1>
                         <div className="list-group" id="myList" role="tablist">
-                            <a className="list-group-item list-group-item-action active" data-toggle="list" href="#home" role="tab">Home</a>
+                            <a className="list-group-item list-group-item-action active" data-toggle="list" href="#staff" role="tab">Персонал</a>
                             <a className="list-group-item list-group-item-action" data-toggle="list" href="#profile" role="tab">Profile</a>
                             <a className="list-group-item list-group-item-action" data-toggle="list" href="#messages" role="tab">Messages</a>
                             <a className="list-group-item list-group-item-action" data-toggle="list" href="#settings" role="tab">Settings</a>
@@ -27,7 +28,7 @@ export default class AdminPage extends React.Component<Props,any> {
 
                     <div className="col-10">
                         <div className="tab-content" >
-                            <div className="tab-pane active " id="home" role="tabpanel" ><DoctorsComponent/> </div>
+                            <div className="tab-pane active " id="staff" role="tabpanel" ><DoctorsComponent/> </div>
                             <div className="tab-pane" id="profile" role="tabpanel">...</div>
                             <div className="tab-pane" id="messages" role="tabpanel">...</div>
                             <div className="tab-pane" id="settings" role="tabpanel">...</div>
