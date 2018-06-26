f2818f5ae17cd47911bf7fc17c0c683581805fdf
alisa2111 <31954373+alisa2111@users.noreply.github.com>
2018-06-26 21:42:54 +0300

    before specialists page

--- src/views/common/AppRouter.tsx
+++ src/views/common/AppRouter.tsx
@@ -8,16 +8,17 @@ import PatientsPage from "./PatientsPage";
 import StaffPage from "../roles/admin/StaffPage";
 import PatientCard from "../roles/admin/PatientCard";
 import ClaimsPage from "../roles/admin/ClaimsPage";
+import DoctorsArchive from "../roles/admin/archive/DoctorsArchive";
 import PatientsArchive from "../roles/admin/archive/PatientsArchive";
 import MyPatientsPage from "../roles/docrtor/MyPatientsPage";
 import AppointmentsPage from "../roles/patient/AppointmentsPage";
 import {Patient} from "../../models/Patient";
+import MyDoctorsPage from "../roles/patient/MyDoctorsPage";
 import MyClaimsPage from "../roles/docrtor/MyClaimsPage";
 import {Doctor} from "../../models/Doctor";
 import DoctorProfile from "./DoctorProfile";
 import DoctorAppointmentsPage from "../roles/docrtor/DoctorAppointmentsPage";
 import config from "../../config";
-import SpecialistsPage from "../experiment/SpecialistsPage";
 
 interface Props{
     user?: User
@@ -31,123 +32,80 @@ export default class AppRouter extends React.Component<Props> {
     render(){
         const {onLogin , user, patient, onPatient, doctor, onDoctor} = this.props;
         if( user && user.isSignedIn && user.role==='DOCTOR' ) {
-           return(
-               <BrowserRouter>
-                   <Switch>
-                       <Route exact={true} path={config.links.APPOINTMENTS}
-                              render={() =>
-                                  <DoctorAppointmentsPage
-                                      onLogin={onLogin}
-                                      user={user}
-                                  />}
-                       />
-                       <Route exact={true} path={config.links.PATIENT_CARD}
-                              render={() =>
-                                  <PatientCard
-                                      user={user}
-                                      onLogin={onLogin}
-                                      patient={patient}
-                                      onPatient={onPatient}
-                                      isDoctor={user.role === 'DOCTOR'}
-                                      isAdmin={user.role === 'ADMIN'}
-                                      isPatient={user.role === 'PATIENT'}
-                                  />}
-                       />
-                       <Route exact={true} path={config.links.PROFILE}
-                              render={() =>
-                                  <DoctorProfile
-                                      user={user}
-                                      onLogin={onLogin}
-                                      doctor={doctor}
-                                      onDoctor={onDoctor}
-                                      isDoctor={user.role === 'DOCTOR'}
-                                      isAdmin={user.role === 'ADMIN'}
-                                      isPatient={user.role === 'PATIENT'}
-                                      self={true}
-                                  />}
-                       />
-                       <Route exact={true} path={config.links.MY_PATIENTS}
-                              render={() =>
-                                  <MyPatientsPage
-                                      onLogin={onLogin}
-                                      user={user}
-                                      onPatient={onPatient}
-                                  />}
-                       />
-                       <Route exact={true} path={config.links.DOCTOR_CLAIMS_FROM_PATIENTS}
-                              render={() =>
-                                  <MyClaimsPage
-                                      onLogin={onLogin}
-                                      user={user}
-                                  />}
-                       />
-                       <Route exact={true} path={config.links.PATIENTS}
-                              render={() =>
-                                  <PatientsPage
-                                       onLogin={onLogin}
-                                       onPatient={onPatient}
-                                       user={user}
-                                       isDoctor={true}
-                                  />
-                       }/>
-                       <Redirect to={config.links.MY_PATIENTS}/>
-                   </Switch>
-               </BrowserRouter>
-           )
+            return(
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact={true} path={config.links.APPOINTMENTS}
+                               render={() => <DoctorAppointmentsPage onLogin={onLogin} user={user}/>}/>
+                        <Route exact={true} path={config.links.PATIENT_CARD} render={() =>
+                            <PatientCard
+                                user={user}
+                                onLogin={onLogin}
+                                patient={patient}
+                                onPatient={onPatient}
+                                isDoctor={user.role === 'DOCTOR'}
+                                isAdmin={user.role === 'ADMIN'}
+                                isPatient={user.role === 'PATIENT'}
+                            />}/>
+
+                        <Route exact={true} path={config.links.PROFILE} render={() =>
+                            <DoctorProfile
+                                user={user}
+                                onLogin={onLogin}
+                                doctor={doctor}
+                                onDoctor={onDoctor}
+                                isDoctor={user.role === 'DOCTOR'}
+                                isAdmin={user.role === 'ADMIN'}
+                                isPatient={user.role === 'PATIENT'}
+                                self={true}
+                            />}/>
+                        <Route exact={true} path={config.links.MY_PATIENTS}
+                               render={() => <MyPatientsPage onLogin={onLogin} user={user} onPatient={onPatient}/>}/>
+                        <Route exact={true} path={config.links.DOCTOR_CLAIMS_FROM_PATIENTS}
+                               render={() => <MyClaimsPage onLogin={onLogin} user={user}/>}/>
+                        <Route exact={true} path={config.links.PATIENTS} render={() =>
+                            <PatientsPage
+                                onLogin={onLogin}
+                                onPatient={onPatient}
+                                user={user}
+                                isDoctor={true}
+                            />
+                        }/>
+                        <Redirect to={config.links.MY_PATIENTS}/>
+                    </Switch>
+                </BrowserRouter>
+            )
         }
 
         if( user &&  user.isSignedIn && user.role==='PATIENT' ) {
             return(
                 <BrowserRouter>
                     <Switch>
-                        <Route exact={true} path={config.links.PATIENT_CARD}
-                               render={() =>
-                                   <PatientCard
-                                       user={user}
-                                       onLogin={onLogin}
-                                       patient={patient}
-                                       onPatient={onPatient}
-                                       isPatient={user.role === 'PATIENT'}
-                                   />}
-                        />
-                        <Route exact={true} path={config.links.PROFILE}
-                               render={() =>
-                                   <DoctorProfile
-                                       user={user}
-                                       onLogin={onLogin}
-                                       doctor={doctor}
-                                       onDoctor={onDoctor}
-                                       isDoctor={user.role === 'DOCTOR'}
-                                       isAdmin={user.role === 'ADMIN'}
-                                       isPatient={user.role === 'PATIENT'}
-                                   />}
-                        />
+                        <Route exact={true} path={config.links.PATIENT_CARD} render={() =>
+                            <PatientCard
+                                user={user}
+                                onLogin={onLogin}
+                                patient={patient}
+                                onPatient={onPatient}
+                                isPatient={user.role === 'PATIENT'}
+                            />}/>
+
+                        <Route exact={true} path={config.links.PROFILE} render={() =>
+                            <DoctorProfile
+                                user={user}
+                                onLogin={onLogin}
+                                doctor={doctor}
+                                onDoctor={onDoctor}
+                                isDoctor={user.role === 'DOCTOR'}
+                                isAdmin={user.role === 'ADMIN'}
+                                isPatient={user.role === 'PATIENT'}
+                            />}/>
                         <Route exact={true} path={config.links.DOCTORS}
-                               render={() =>
-                                   <SpecialistsPage
-                                       onLogin={onLogin}
-                                       user={user}
-                                       onDoctor={onDoctor}
-                                       isDoctorsForPatient={true}
-                                   />}
-                        />
+                               render={() => <DoctorsPage onLogin={onLogin} user={user} isPatient={true} onDoctor={onDoctor}/>}/>
                         <Route exact={true} path={config.links.MY_DOCTORS}
-                               render={() =>
-                                   <SpecialistsPage
-                                       onLogin={onLogin}
-                                       user={user}
-                                       onDoctor={onDoctor}
-                                       isDoctorsForPatient={true}
-                                       isMyDoctor={true}
-                                   />}
-                        />
+                               render={() => <MyDoctorsPage onLogin={onLogin} user={user} onDoctor={onDoctor}/>}/>
                         <Route exact={true} path={config.links.PATIENT_APPOINTMENTS}
-                               render={() =>
-                                   <AppointmentsPage
-                                       onLogin={onLogin}
-                                       user={user}
-                                   />}
-                        />
+                               render={() => <AppointmentsPage onLogin={onLogin} user={user}/>}/>
                         <Redirect exact={true} to={config.links.DOCTORS}/>
                     </Switch>
                 </BrowserRouter>
@@ -158,82 +116,48 @@ export default class AppRouter extends React.Component<Props> {
             return (
                 <BrowserRouter>
                     <Switch>
-                        <Route exact={true} path={config.links.PROFILE}
-                               render={() =>
-                                   <DoctorProfile
-                                       user={user}
-                                       onLogin={onLogin}
-                                       doctor={doctor}
-                                       onDoctor={onDoctor}
-                                       isDoctor={user.role === 'DOCTOR'}
-                                       isAdmin={user.role === 'ADMIN'}
-                                       isPatient={user.role === 'PATIENT'}
-                                   />}
-                        />
-                        <Route exact={true} path={config.links.STAFF}
-                               render={() =>
-                                   <StaffPage
-                                       onDoctor={onDoctor}
-                                       onLogin={onLogin}
-                                       user={user}
-                                   />}
-                        />
-                        <Route exact={true} path={config.links.PATIENTS}
-                               render={() =>
-                                   <PatientsPage
-                                       onLogin={onLogin}
-                                       onPatient={onPatient}
-                                       user={user}
-                                   />}
-                        />
+                        <Route exact={true} path={config.links.PROFILE} render={() =>
+                            <DoctorProfile
+                                user={user}
+                                onLogin={onLogin}
+                                doctor={doctor}
+                                onDoctor={onDoctor}
+                                isDoctor={user.role === 'DOCTOR'}
+                                isAdmin={user.role === 'ADMIN'}
+                                isPatient={user.role === 'PATIENT'}
+                            />}/>
+                        <Route exact={true} path={config.links.STAFF} render={() => <StaffPage onDoctor={onDoctor} onLogin={onLogin} user={user}/>}/>
+                        <Route exact={true} path={config.links.PATIENTS} render={() =>
+                            <PatientsPage
+                                onLogin={onLogin}
+                                onPatient={onPatient}
+                                user={user}/>}/>
                         <Route exact={true} path={config.links.CLAIMS}
-                               render={() =>
-                                   <ClaimsPage
-                                       onLogin={onLogin}
-                                       user={user}
-                                   />}
-                        />
-                        <Route exact={true} path={config.links.PATIENT_CARD}
-                               render={() =>
-                                   <PatientCard
-                                       user={user}
-                                       onLogin={onLogin}
-                                       patient={patient}
-                                       onPatient={onPatient}
-                                       isDoctor={user.role === 'DOCTOR'}
-                                       isAdmin={user.role === 'ADMIN'}
-                                       isPatient={user.role === 'PATIENT'}
-                                   />}
-                        />
+                               render={() => <ClaimsPage onLogin={onLogin} user={user}/>}/>
+                        <Route exact={true} path={config.links.PATIENT_CARD} render={() =>
+                            <PatientCard
+                                user={user}
+                                onLogin={onLogin}
+                                patient={patient}
+                                onPatient={onPatient}
+                                isDoctor={user.role === 'DOCTOR'}
+                                isAdmin={user.role === 'ADMIN'}
+                                isPatient={user.role === 'PATIENT'}
+                            />}/>
                         <Route exact={true} path={config.links.ARCHIVE_DOCTORS}
-                               render={() =>
-                                   <SpecialistsPage
-                                       onLogin={onLogin}
-                                       user={user}
-                                       onDoctor={onDoctor}
-                                       isDoctorsInArchive={true}
-                                   />}
-                        />
+                               render={() => <DoctorsArchive onLogin={onLogin} user={user} onDoctor={onDoctor}/>}/>
                         <Route exact={true} path={config.links.ARCHIVE_PATIENTS}
-                               render={() =>
-                                   <PatientsArchive
-                                       onLogin={onLogin}
-                                       user={user}
-                                       onPatient={onPatient}
-                                   />}
-                        />
-                        <Route exact={true} path={config.links.PROFILE}
-                               render={() =>
-                                   <DoctorProfile
-                                       user={user}
-                                       onLogin={onLogin}
-                                       doctor={doctor}
-                                       onDoctor={onDoctor}
-                                       isDoctor={user.role === 'DOCTOR'}
-                                       isAdmin={user.role === 'ADMIN'}
-                                       isPatient={user.role === 'PATIENT'}
-                                   />}
-                        />
+                               render={() => <PatientsArchive onLogin={onLogin} user={user} onPatient={onPatient}/>}/>
+                        <Route exact={true} path={config.links.PROFILE} render={() =>
+                            <DoctorProfile
+                                user={user}
+                                onLogin={onLogin}
+                                doctor={doctor}
+                                onDoctor={onDoctor}
+                                isDoctor={user.role === 'DOCTOR'}
+                                isAdmin={user.role === 'ADMIN'}
+                                isPatient={user.role === 'PATIENT'}
+                            />}/>
                         <Redirect exact={true} to={config.links.STAFF}/>
                     </Switch>
                 </BrowserRouter>
@@ -244,22 +168,8 @@ export default class AppRouter extends React.Component<Props> {
             return (
                 <BrowserRouter>
                     <Switch>
-                        <Route exact={true} path={config.links.DOCTORS}
-                               render={()=>
-                                   <DoctorsPage
-                                       onDoctor={onDoctor}
-                                       onLogin={onLogin}
-                                       user={user}
-                                       isAdmin={false}
-                                   />}
-                        />
-                        <Route exact={true} path='*'
-                               render={() =>
-                                   <HomePage
-                                       onLogin={onLogin}
-                                       user={user}
-                                   />}
-                        />
+                        <Route exact={true} path={config.links.DOCTORS} render={()=><DoctorsPage onDoctor={onDoctor} onLogin={onLogin} user={user} isAdmin={false}/>}/>
+                        <Route exact={true} path='*' render={() =>  <HomePage onLogin={onLogin} user={user}/>}/>
                     </Switch>
                 </BrowserRouter>
             )
--- src/views/roles/patient/MyDoctorsPage.tsx
+++ src/views/roles/patient/MyDoctorsPage.tsx
@@ -1,102 +1,102 @@
-// import * as React from 'react';
-// import {User} from "../../../models/User";
-// import Header from "../../common/Header";
-// import {img_doctor} from "../../../data/doctor";
-// import {CardView} from "../../common/CardView";
-// import {Doctor} from "../../../models/Doctor";
-// import config from "../../../../config";
-//
-// interface Props{
-//     user: User
-//     onLogin: (user: User) => void
-//     onDoctor: (doctor: Doctor) => void
-// }
-// export default class MyDoctorsPage extends React.Component<Props,any> {
-//     constructor(props: any) {
-//         super(props);
-//         this.state = {
-//             doctors: []
-//         };
-//     }
-//
-//     refreshDoctors = (result: any) => {
-//         const doctors = result.map((e: any) => new Doctor(
-//             e.id,
-//             e.lastName,
-//             e.firstName,
-//             e.secondName,
-//             e.email,
-//             e.password,
-//             e.department,
-//             e.specialty,
-//             e.firstPractiseDate
-//         ));
-//         this.setState({doctors})
-//     };
-//
-//     componentWillMount() {
-//         const {id} = this.props.user;
-//         fetch(config.urls.PATIENT_GET_DOCTORS, {
-//             method: 'post',
-//             headers: {
-//                 'Content-Type': `application/x-www-form-urlencoded`
-//             },
-//             body: "id=" + id
-//         })
-//             .then((res: any) => {
-//                 return res.json();
-//             })
-//             .then(this.refreshDoctors)
-//             .catch((err: any) => {
-//                 console.log(err)
-//             })
-//     }
-//
-//     groupBy(arr: any, key: any) {
-//         return arr.reduce(function (rv: any, x: any) {
-//             (rv[x[key]] = rv[x[key]] || []).push(x);
-//             return rv;
-//         }, {});
-//     };
-//
-//
-//     render(){
-//         const {onLogin, user, onDoctor} = this.props;
-//         const {doctors} = this.state;
-//         const groupedCards = this.groupBy(doctors, 'department');  //groped by department
-//         const groups = Object.keys(groupedCards).map((department, index) => {
-//             return (
-//                 <div>
-//                     <div className='row'>
-//                         <div className='col-12'>
-//                             <div className='header border border-dark' key={index}>{department + " отделение"}</div>
-//                         </div>
-//                     </div>
-//                     <div className='card-deck'>
-//                         {groupedCards[department].map((doctor: object) => {
-//                             return (
-//                                 <CardView
-//                                     data={doctor}
-//                                     onDoctor={onDoctor}
-//                                     refreshDoctorsArchive={this.refreshDoctors}
-//                                     image={img_doctor}
-//                                     isPatient={true}
-//                                     myDoctor={true}
-//                                     refreshDoctors={this.refreshDoctors}
-//                                 />
-//                             )
-//                         })}
-//                     </div>
-//                 </div>
-//             )
-//         });
-//         return(
-//             <div className="container-fluid">
-//                 <Header onLogin={onLogin} user={user} isPatient={true}/>
-//                 {groups}
-//             </div>
-//
-//         )
-//     }
-// }
-//
+import * as React from 'react';
+import {User} from "../../../models/User";
+import Header from "../../common/Header";
+import {img_doctor} from "../../../data/doctor";
+import {CardView} from "../../common/CardView";
+import {Doctor} from "../../../models/Doctor";
+import config from "../../../config";
+
+interface Props{
+    user: User
+    onLogin: (user: User) => void
+    onDoctor: (doctor: Doctor) => void
+}
+export default class MyDoctorsPage extends React.Component<Props,any> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            doctors: []
+        };
+    }
+
+    refreshDoctors = (result: any) => {
+        const doctors = result.map((e: any) => new Doctor(
+            e.id,
+            e.lastName,
+            e.firstName,
+            e.secondName,
+            e.email,
+            e.password,
+            e.department,
+            e.specialty,
+            e.firstPractiseDate
+        ));
+        this.setState({doctors})
+    };
+
+    componentWillMount() {
+        const {id} = this.props.user;
+        fetch(config.urls.PATIENT_GET_DOCTORS, {
+            method: 'post',
+            headers: {
+                'Content-Type': `application/x-www-form-urlencoded`
+            },
+            body: "id=" + id
+        })
+            .then((res: any) => {
+                return res.json();
+            })
+            .then(this.refreshDoctors)
+            .catch((err: any) => {
+                console.log(err)
+            })
+    }
+
+    groupBy(arr: any, key: any) {
+        return arr.reduce(function (rv: any, x: any) {
+            (rv[x[key]] = rv[x[key]] || []).push(x);
+            return rv;
+        }, {});
+    };
+
+
+    render(){
+        const {onLogin, user, onDoctor} = this.props;
+        const {doctors} = this.state;
+        const groupedCards = this.groupBy(doctors, 'department');  //groped by department
+        const groups = Object.keys(groupedCards).map((department, index) => {
+            return (
+                <div>
+                    <div className='row'>
+                        <div className='col-12'>
+                            <div className='header border border-dark' key={index}>{department + " отделение"}</div>
+                        </div>
+                    </div>
+                    <div className='card-deck'>
+                        {groupedCards[department].map((doctor: object) => {
+                            return (
+                                <CardView
+                                    data={doctor}
+                                    onDoctor={onDoctor}
+                                    refreshDoctorsArchive={this.refreshDoctors}
+                                    image={img_doctor}
+                                    isPatient={true}
+                                    myDoctor={true}
+                                    refreshDoctors={this.refreshDoctors}
+                                />
+                            )
+                        })}
+                    </div>
+                </div>
+            )
+        });
+        return(
+            <div className="container-fluid">
+                <Header onLogin={onLogin} user={user} isPatient={true}/>
+                {groups}
+            </div>
+
+        )
+    }
+}
+
